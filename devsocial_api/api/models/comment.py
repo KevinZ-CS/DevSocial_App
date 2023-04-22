ac84cfce377bc2f9bfd3e4cd25dcf4a74acba21b
Kevin Zheng <kevzheng27@gmail.com>
2023-04-22 15:26:50 -0400

    Add custom model validation error messages to remaining models

--- devsocial_api/api/models/comment.py
+++ devsocial_api/api/models/comment.py
@@ -2,7 +2,22 @@ from django.db import models
 from api.models import User
 from api.models import Post
 
+def generate_error_messages(attribute, messages):
+    error_dict = {}
+    for key, message in messages.items():
+        error_dict[key] = message.format(attribute=attribute)
+    return error_dict
+
+blank_null_error_messages = {
+    'blank': _('The {attribute} field cannot be be blank.'),
+    'null': _('The {attribute} field cannot be null.')
+}
+
 class Comment(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE, blank=False, null=False)
-    comment = models.CharField(blank=False, null=False)
+    comment = models.CharField(
+        blank=False, 
+        null=False,
+        error_messages=generate_error_messages('comment', blank_null_error_messages)
+    )
--- devsocial_api/api/serializers/user_serializer.py
+++ devsocial_api/api/serializers/user_serializer.py
@@ -9,16 +9,3 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
     
-    def validate(self, data):
-        errors = {}
-
-        for field_name, value in data.items():
-            try:
-                validate_not_blank(value)
-            except ValidationError as error:
-                errors[field_name] = error.message
-
-        if errors:
-            raise serializers.ValidationError(errors)
-
-        return data
